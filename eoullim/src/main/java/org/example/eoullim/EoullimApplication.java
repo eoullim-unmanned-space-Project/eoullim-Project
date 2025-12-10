6e826cdd1849b398091f974074cf4d56494bf43b
nolang22 <rnrmfwwkd22@gmail.com>
2025-12-10 16:16:40 +0900

    created back files/lee

---
+++ eoullim/src/main/java/org/example/eoullim/EoullimApplication.java
@@ -0,0 +1,13 @@
+package org.example.eoullim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoullimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EoullimApplication.class, args);
+    }
+
+}
