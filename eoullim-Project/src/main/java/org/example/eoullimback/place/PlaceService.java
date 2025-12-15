673b833c8dbfa5705492a465227a09eeeccffde5
nolang22 <rnrmfwwkd22@gmail.com>
2025-12-15 16:05:36 +0900

    [feature] 파트:B 컨트롤러, 서비스임플리 개발

---
+++ eoullim-Project/src/main/java/org/example/eoullimback/place/PlaceService.java
@@ -0,0 +1,4 @@
+package org.example.eoullimback.place;
+
+public interface PlaceService {
+}
--- eoullim-Project/src/main/java/org/example/eoullimback/timeslot/TimeSlotServiceImpl.java
+++ eoullim-Project/src/main/java/org/example/eoullimback/timeslot/TimeSlotServiceImpl.java
@@ -1,4 +1,11 @@
 package org.example.eoullimback.timeslot;
 
-public class TimeSlotServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TimeSlotServiceImpl implements TimeSlotService {
 }
