b7713bf7df050460bc475347615a3821a4bd26e2
AKBINJ <jyp10311@naver.com>
2025-12-16 17:08:54 +0900

    [feat] comment Dto

--- eoullim-Project/src/main/java/org/example/eoullimback/qaa/QaaController.java
+++ eoullim-Project/src/main/java/org/example/eoullimback/qaa/QaaController.java
@@ -4,21 +4,22 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.eoullimback.user_auth.user.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class QaaController {
 
-    private final QaaRepository qaaRepository;
+    private final QaaService qaaService;
 
-    @PostMapping("/qaa")
-    public String createQaa(HttpSession session) {
-        User sesionUser = (User)session.getAttribute("sessionUser");
-
-//        Qaa qaa = saveDto.toEntity(sessionUser);
-//        qaaRepository.save(qaa);
-
-        return "redirect:/qaa/list";
+    // Q&A 화면 요청
+    @GetMapping("/qaas/list")
+    public String qaaList(Model model) {
+//        List<QaaResponse.QaaListDto> qaaList = qaaService.qaaLists();
+//        model.addAttribute("qaaList", qaaList);
+        return "qaa/list";
     }
 }
