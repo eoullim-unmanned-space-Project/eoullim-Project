c52061287db873765192f9e73be3fd5e0145ea53
cgung7 <cgung7@naver.com>
2025-12-24 09:09:37 +0900

    [refactor] 유저 및 회원코드 리펙터링

--- eoullim-Project/src/main/java/org/example/eoullimback/user_auth/user/UserController.java
+++ eoullim-Project/src/main/java/org/example/eoullimback/user_auth/user/UserController.java
@@ -13,46 +13,47 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+//    private final Long userId = 1L;
 
-    // // http://localhost:8080/users/me
-    // 유저 컨트롤러
+    // // http://localhost:8080/users/me/1
+    // 유저 컨트롤러 , @PathVariable Long id,
     @GetMapping("/me/{id}")
-    public String myProfile(@RequestParam("userId") Long userId, Model model) {
+    public String myProfile(@PathVariable Long id, Model model) {
 
-        User user = userService.getMyProfile(userId);
+        User user = userService.getMyProfile(id);
 
         model.addAttribute("user", user);
 
-        return "users/profile";
+        return "user/profile";
     }
 
     /**
      * 회원 수정(프로필 생성)
      */
     @PostMapping("/me/{id}")
-    public String updateProfile(@RequestParam("userId") Long id, @ModelAttribute @Valid UserRequest.UpDateDTO update) {
+    public String updateProfile(@PathVariable Long id, @ModelAttribute @Valid UserRequest.UpDateDTO update) {
 
         userService.updateProfile(id, update);
 
-        return "redirect:/users/me?userId="+ id;
+        return "redirect:users/me/"+ id;
     }
 
     /**
      * 회원 수정(프로필 삭제)
      */
     @PostMapping("/profile-image/delete")
-    public String deleteProfileImage(@RequestParam("userId") Long id) {
+    public String deleteProfileImage(@PathVariable Long id) {
 
         userService.deleteProfileImage(id);
 
-        return "redirect:/users/me";
+        return "redirect:user/me";
     }
 
     /**
      * 회원 탈퇴
      */
     @PostMapping("/leave")
-    public String leaveUser(@RequestParam("userId") Long id) {
+    public String leaveUser(@PathVariable Long id) {
 
         userService.leaveUser(id);
 
