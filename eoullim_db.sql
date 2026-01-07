85a074c2ac84fdf7c17e36015e9180cca4e8350b
tndals11 <136726862+tndals11@users.noreply.github.com>
2026-01-07 09:46:56 +0900

    [feat] footer 구현
    사용자 웹 페이지 footer 구현 완료

--- eoullim_db.sql
+++ eoullim_db.sql
@@ -6,7 +6,7 @@ USE eoullim_db;
 
 SELECT * FROM time_slots;
 SELECT * FROM items;
-SELECT * FROM places;
+SELECT * FROM places;	
 SELECT * FROM rooms;
 SELECT * FROM users;
 SELECT * FROM bookings;
@@ -316,12 +316,15 @@ CREATE TABLE notifications (
   type VARCHAR(20) NOT NULL COMMENT '알림(PAYMENT, REFUND, CANCEL, BOOKING)',
   status VARCHAR(20) NOT NULL DEFAULT 'PENDING' COMMENT '전송 상태(PENDING, SENT, FAILED)',
   message VARCHAR(255) NOT NULL COMMENT '전송 메시지',
-  qr_code VARCHAR(100) NULL COMMENT 'QR 코드 토큰',
+  qr_code VARCHAR(40) NULL COMMENT 'QR 코드 토큰',
   created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   sent_at DATETIME(6) NULL COMMENT '전송 시각',
   
   CHECK (type IN('PAYMENT','REFUND','CANCEL','BOOKING')),
   CHECK (status IN('PENDING','SENT','FAILED')),
+  
+  INDEX `idx_notifications_user_id` (user_id),
+  INDEX `idx_notifications_payment_id` (payment_id),
   
   CONSTRAINT `fk_notifications_user` FOREIGN KEY (user_id) REFERENCES users(id),
   CONSTRAINT `fk_notifications_payment` FOREIGN KEY (payment_id) REFERENCES payments(id)
