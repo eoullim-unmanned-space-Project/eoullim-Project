bca7463272e188f98b14bc7747fdc9e7ea96685b
nolang22 <rnrmfwwkd22@gmail.com>
2026-01-08 17:04:38 +0900

    [feat] room/list 머스테치 작업

--- eoullim-Project/src/main/java/org/example/eoullimback/timeslot/TimeSlotResponse.java
+++ eoullim-Project/src/main/java/org/example/eoullimback/timeslot/TimeSlotResponse.java
@@ -2,6 +2,7 @@ package org.example.eoullimback.timeslot;
 
 import jakarta.persistence.Column;
 import lombok.Data;
+import org.example.eoullimback._common.enums.time_slot.SlotStatus;
 
 import java.time.LocalDateTime;
 
@@ -23,13 +24,18 @@ public class TimeSlotResponse {
 
     @Data
     public static class DetailDTO {
+        private Long timeSlotId;
+
         private LocalDateTime startTime;
 
         private LocalDateTime endTime;
+        private SlotStatus status;
 
         public DetailDTO(TimeSlot timeSlot) {
+            this.timeSlotId = timeSlot.getId();
             this.startTime = timeSlot.getStartTime();
             this.endTime = timeSlot.getEndTime();
+            this.status = timeSlot.getStatus();
         }
     }
 }
