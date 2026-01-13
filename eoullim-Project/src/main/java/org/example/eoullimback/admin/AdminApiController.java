295a3f00965c630499d1dc9fac3aa60094649b41
nolang22 <rnrmfwwkd22@gmail.com>
2026-01-13 17:19:41 +0900

    [feat]장소-수정-삭제-머스테치

--- eoullim-Project/src/main/java/org/example/eoullimback/admin/AdminApiController.java
+++ eoullim-Project/src/main/java/org/example/eoullimback/admin/AdminApiController.java
@@ -4,14 +4,20 @@ import lombok.RequiredArgsConstructor;
 import org.example.eoullimback.payment_refund.PaymentRefundRequest;
 import org.example.eoullimback.payment_refund.PaymentRefundResponse;
 import org.example.eoullimback.payment_refund.PaymentRefundService;
+import org.example.eoullimback.place.Place;
+import org.example.eoullimback.place.PlaceRequest;
+import org.example.eoullimback.place.PlaceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminApiController {
 
     private final PaymentRefundService paymentRefundService;
+    private final PlaceService placeService;
 
     @GetMapping("/admin/refund/detail/{id}")
     public ResponseEntity<PaymentRefundResponse.AdminDetailDTO> detail(@PathVariable Long id) {
@@ -33,6 +39,38 @@ public class AdminApiController {
     public ResponseEntity<Void> approve(@PathVariable Long id) {
 
         paymentRefundService.approve(id);
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    // =====
+    // 장소
+    // ====
+
+    @PostMapping("/place/create")
+    public ResponseEntity<?> createProc(PlaceRequest.CreateDTO request) {
+
+        request.validate();
+        Place place = placeService.placeCreate(request);
+
+        return ResponseEntity.ok().body(place);
+    }
+
+    @PutMapping("/place/{placeId}")
+    public ResponseEntity<Map<String, Place>> UpdateProc(@PathVariable Long placeId,
+                             PlaceRequest.UpdateDTO request
+    ) {
+        request.validate();
+
+        Place place = placeService.placeUpdate(placeId, request);
+
+        return ResponseEntity.ok().body(Map.of("place", place));
+    }
+
+    @DeleteMapping("/place/{id}")
+    public ResponseEntity<Void> deleteProc(@PathVariable(name = "id") Long placeId) {
+
+        placeService.placeDelete(placeId);
 
         return ResponseEntity.ok().body(null);
     }
